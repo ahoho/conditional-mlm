e2e03fd660d5a7b7a785dc26cd3ada5243f216e2
ahoho <ahoyle@wesleyan.edu>
2020-07-28 11:58:29 -0700

    working conditional mlm training

--- run_conditional_mlm.py
+++ run_conditional_mlm.py
@@ -1,30 +1,16 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """
 Fine-tuning the library models for language modeling on a text file (GPT, GPT-2, CTRL, BERT, RoBERTa, XLNet).
 GPT, GPT-2 and CTRL are fine-tuned using a causal language modeling (CLM) loss. BERT and RoBERTa are fine-tuned
 using a masked language modeling (MLM) loss. XLNet is fine-tuned using a permutation language modeling (PLM) loss.
 """
 
-
+import json
 import logging
 import math
 import os
 from dataclasses import dataclass, field
 from typing import Optional
+from pathlib import Path
 
 from transformers import (
     CONFIG_MAPPING,
@@ -32,16 +18,14 @@ from transformers import (
     AutoConfig,
     AutoModelWithLMHead,
     AutoTokenizer,
-    DataCollatorForLanguageModeling,
-    DataCollatorForPermutationLanguageModeling,
     HfArgumentParser,
-    LineByLineTextDataset,
     PreTrainedTokenizer,
-    TextDataset,
     Trainer,
     TrainingArguments,
     set_seed,
 )
+
+from data import LineByLinePairedTextDataset, DataCollatorForConditionalMLM
 
 
 logger = logging.getLogger(__name__)
@@ -83,33 +67,30 @@ class DataTrainingArguments:
     """
     Arguments pertaining to what data we are going to input our model for training and eval.
     """
+    data_dir: str = field(metadata={"help": "Input data directory"})
+    train_source_file: Optional[str] = field(
+        default="train.source",
+        metadata={"help": "The input training source data file (a text file)."}
+    )
+    train_target_file: Optional[str] = field(
+        default="train.target",
+        metadata={"help": "The input training target data file (a text file)."}
+    )
+    eval_target_file: Optional[str] = field(
+        default="val.source",
+        metadata={"help": "An optional input evaluation source file to evaluate the perplexity on (a text file)."},
+    )
+    eval_source_file: Optional[str] = field(
+        default="val.target",
+        metadata={"help": "An optional input evaluation target file to evaluate the perplexity on (a text file)."},
+    )
 
-    train_data_file: Optional[str] = field(
-        default=None, metadata={"help": "The input training data file (a text file)."}
-    )
-    eval_data_file: Optional[str] = field(
-        default=None,
-        metadata={"help": "An optional input evaluation data file to evaluate the perplexity on (a text file)."},
-    )
-    line_by_line: bool = field(
-        default=False,
-        metadata={"help": "Whether distinct lines of text in the dataset are to be handled as distinct sequences."},
+    added_tokens_file: Optional[str] = field(
+        default=None, metadata={"help": "Location of added tokens"},
     )
 
-    mlm: bool = field(
-        default=False, metadata={"help": "Train with masked-language modeling loss instead of language modeling."}
-    )
     mlm_probability: float = field(
         default=0.15, metadata={"help": "Ratio of tokens to mask for masked language modeling loss"}
-    )
-    plm_probability: float = field(
-        default=1 / 6,
-        metadata={
-            "help": "Ratio of length of a span of masked tokens to surrounding context length for permutation language modeling."
-        },
-    )
-    max_span_length: int = field(
-        default=5, metadata={"help": "Maximum length of a span of masked tokens for permutation language modeling."}
     )
 
     block_size: int = field(
@@ -126,13 +107,14 @@ class DataTrainingArguments:
 
 
 def get_dataset(args: DataTrainingArguments, tokenizer: PreTrainedTokenizer, evaluate=False):
-    file_path = args.eval_data_file if evaluate else args.train_data_file
-    if args.line_by_line:
-        return LineByLineTextDataset(tokenizer=tokenizer, file_path=file_path, block_size=args.block_size)
-    else:
-        return TextDataset(
-            tokenizer=tokenizer, file_path=file_path, block_size=args.block_size, overwrite_cache=args.overwrite_cache
-        )
+    source_file = args.eval_source_file if evaluate else args.train_source_file
+    target_file = args.eval_target_file if evaluate else args.train_target_file
+    return LineByLinePairedTextDataset(
+        tokenizer=tokenizer,
+        source_file_path=Path(args.data_dir, source_file),
+        target_file_path=Path(args.data_dir, target_file),
+        block_size=args.block_size
+    )
 
 
 def main():
@@ -143,9 +125,10 @@ def main():
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    if data_args.eval_data_file is None and training_args.do_eval:
+    if data_args.eval_source_file is None and training_args.do_eval:
         raise ValueError(
-            "Cannot do evaluation without an evaluation data file. Either supply a file to --eval_data_file "
+            "Cannot do evaluation without an evaluation data file. "
+            "Either supply a file to --eval_source_file and --eval_target_file"
             "or remove the --do_eval argument."
         )
 
@@ -213,13 +196,16 @@ def main():
         logger.info("Training new model from scratch")
         model = AutoModelWithLMHead.from_config(config)
 
+    # add added tokens
+    if data_args.added_tokens_file is not None:
+        with open(data_args.added_tokens_file, "r") as infile:
+            added_tokens = json.load(infile)
+    tokenizer.add_tokens(added_tokens)
+
     model.resize_token_embeddings(len(tokenizer))
 
-    if config.model_type in ["bert", "roberta", "distilbert", "camembert"] and not data_args.mlm:
-        raise ValueError(
-            "BERT and RoBERTa-like models do not have LM heads but masked LM heads. They must be run using the"
-            "--mlm flag (masked language modeling)."
-        )
+    if config.model_type not in ["bert", "roberta", "distilbert", "camembert"]:
+        raise ValueError("Need to use a masked language model (BERT, RoBERTa, etc.)")
 
     if data_args.block_size <= 0:
         data_args.block_size = tokenizer.max_len
@@ -231,14 +217,10 @@ def main():
 
     train_dataset = get_dataset(data_args, tokenizer=tokenizer) if training_args.do_train else None
     eval_dataset = get_dataset(data_args, tokenizer=tokenizer, evaluate=True) if training_args.do_eval else None
-    if config.model_type == "xlnet":
-        data_collator = DataCollatorForPermutationLanguageModeling(
-            tokenizer=tokenizer, plm_probability=data_args.plm_probability, max_span_length=data_args.max_span_length,
-        )
-    else:
-        data_collator = DataCollatorForLanguageModeling(
-            tokenizer=tokenizer, mlm=data_args.mlm, mlm_probability=data_args.mlm_probability
-        )
+    data_collator = DataCollatorForConditionalMLM(
+        tokenizer=tokenizer,
+        mlm_probability=data_args.mlm_probability
+    )
 
     # Initialize our Trainer
     trainer = Trainer(
